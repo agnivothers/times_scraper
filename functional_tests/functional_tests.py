8657289987c2f6cae6b428417dbcbd5c5d24ecf3
Agniv Adhikari <agniv.others@gmail.com>
2019-09-15 19:15:56 +0530

    Failing test as reminder to complete the test.

--- functional_tests/functional_tests.py
+++ functional_tests/functional_tests.py
@@ -11,7 +11,7 @@ class LoginTest(unittest.TestCase):
     def test_login(self):
         self.browser.get("https://epaper.timesgroup.com/TOI/TimesOfIndia/indialogin.aspx")
         self.assertIn('', self.browser.title)
-        #self.fail('Finish the test!')
+        self.fail('Finish the test!')
 
 if __name__=='__main__':
     unittest.main()
