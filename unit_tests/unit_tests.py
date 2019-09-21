194330e4de9ff434c79c7d7b12cebebe75b5902e
Agniv Adhikari <agniv.others@gmail.com>
2019-09-21 11:49:51 +0530

    Added setUp method. Used object set in setUp method for other tests.

--- unit_tests/unit_tests.py
+++ unit_tests/unit_tests.py
@@ -1,16 +1,20 @@
 import unittest
 from selenium import webdriver
 from code import times_scraper
-#from code.times_scraper import get_browser_firefox
+
 
 
 class TimesHomePageTest(unittest.TestCase):
 
-    def test_times_archive_page_url_load(self):
+    def setUp(self):
         ts = times_scraper.TimesScraper()
-        browser = ts.get_browser()
-        browser = ts.get_times_archive_home_page(browser)
+        self.ts = ts
+
+    def test_times_archive_page_url_load(self):
+        browser = self.ts.get_browser()
+        browser = self.ts.get_times_archive_home_page(browser)
         self.assertIn('', browser.title)
+
 
 if __name__ == '__main__': 
     unittest.main() 
