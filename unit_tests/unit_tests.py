14eb8587f12c1d51527e232ec51c5b3458535293
Agniv Adhikari <agniv.others@gmail.com>
2019-09-21 10:51:32 +0530

    Renamed variable.

--- unit_tests/unit_tests.py
+++ unit_tests/unit_tests.py
@@ -9,8 +9,8 @@ class TimesHomePageTest(unittest.TestCase):
     def test_times_archive_page_url_load(self):
         ts = times_scraper.TimesScraper()
         browser = ts.get_browser()
-        browser2 = ts.get_times_archive_home_page(browser)
-        self.assertIn('', browser2.title)
+        browser = ts.get_times_archive_home_page(browser)
+        self.assertIn('', browser.title)
 
 if __name__ == '__main__': 
     unittest.main() 
