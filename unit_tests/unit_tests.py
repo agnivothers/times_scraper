3441bc1910e9165381127a7c595f4ea931d2ee83
Agniv Adhikari <agniv.others@gmail.com>
2019-10-01 08:13:37 +0530

    All changes till 30.09.2019. Stopping it here.

--- unit_tests/unit_tests.py
+++ unit_tests/unit_tests.py
@@ -1,6 +1,7 @@
 import unittest
 from selenium import webdriver
 from code import times_scraper
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 
@@ -25,12 +26,28 @@ class TimesHomePageTest(unittest.TestCase):
         browser = self.ts.get_times_archive_home_page(browser)
         #a = browser.find_element_by_partial_link_text("Log")
         a = browser.find_elements_by_class_name("btn.btn-primary")
+
         for element in a:
             element.click()
+
         #a = browser.find_elements_by_class_name("col-md-12")
         #a = browser.find_element_by_link_text("Log in here")
         #a = browser.find_elements_by_xpath("//div")
         print(a)
-        self.assertIn('Log In', browser.title)
+        print(browser.window_handles)
+        WebDriverWait(browser, 10).until(lambda d: len(d.window_handles) == 2)
+        for handle in browser.window_handles:
+            print("Printing handles")
+            print(handle)
+            browser.switch_to.window(handle)
+            print(browser.title)
+            WebDriverWait(browser, 10).until(lambda d: d.title != "")
+            self.assertIn('Log In', browser.title)
+            """
+            b = browser.switch_to.window(handle)
+            print('Switched window')
+            print(b)
+            """
+
 if __name__ == '__main__': 
     unittest.main() 
